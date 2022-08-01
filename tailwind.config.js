250395384a08a4fd5452b511c8e89e9ba90b5e94
danbiilee <danbi.db@gmail.com>
2022-08-01 21:11:53 +0900

    chore: Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.[jt]sx?$', './components/**/*.[jt]sx?$'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
