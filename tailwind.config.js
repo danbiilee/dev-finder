07010eba796e244578995447227b4d27bab63f17
danbiilee <danbi.db@gmail.com>
2022-08-03 22:14:46 +0900

    feat: Add basic global style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
   content: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          400: '#8f95a5',
+        },
+        blue: {
+          600: '#0079fe',
+          700: '#1d56a6',
+        },
+        indigo: {
+          800: '#1f2a48',
+          900: '#141c2f',
+        },
+      },
+    },
   },
   plugins: [],
 };
